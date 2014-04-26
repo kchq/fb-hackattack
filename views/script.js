0d118327d41ae15902e195ca4c1e83a63233d599
studentuser <studentuser@studentusers-MacBook-Pro-4.local>
2014-04-25 22:03:08 -0700

    fuck you

---
+++ views/script.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+};
+
+// process response from text box
+function processResponse() {
+  var target = document.getElementById("response");
+  console.log(target.value);
+  var story = document.getElementById("story_box").lastChild;
+  story.innerHTML = target.value;
+  target.value = "";
+}
